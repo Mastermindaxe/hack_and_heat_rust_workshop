3adc7c3033d930602dbdb1643cdc944b5cf97156
Florian Nagel <florian.nagel@pm.me>
2024-09-05 16:23:19 +0200

    Committed Cargo.locks and also added package build instructions for every sub package

--- flake.nix
+++ flake.nix
@@ -10,14 +10,19 @@
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
+      in {
+        packages.default = naersk-lib.buildPackage ./part_2/benchmarks;
+        packages.benchmarks = naersk-lib.buildPackage ./part_2/benchmarks;
+        packages.doc_tests = naersk-lib.buildPackage ./part_2/doc_tests;
+        packages.macros_and_meta_programming =
+          naersk-lib.buildPackage ./part_2/macros_and_meta_programming;
+
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs =
+              [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
         formatter = pkgs.nixfmt;
-      }
-    );
+      });
 }
