2f4e6970c4d2e7bcd3a2a361285c764e7270df6b
Florian Nagel <florian.nagel@pm.me>
2024-09-06 09:51:03 +0200

    Added check steps to flake config

--- flake.nix
+++ flake.nix
@@ -17,6 +17,22 @@
         packages.macros_and_meta_programming =
           naersk-lib.buildPackage ./part_2/macros_and_meta_programming;
 
+        checks = {
+            test = naersk-lib.buildPackage {
+                src = ./part_2;
+                mode = "test";
+            };
+            clippy = naersk-lib.buildPackage {
+                src = ./part_2;
+                mode = "clippy";
+            };
+            format = naersk-lib.buildPackage {
+                src = ./part_2;
+                mode = "fmt";
+                nativeBuildInputs = [ pkgs.rustfmt ];
+            };
+        };
+
         devShells.default = with pkgs;
           mkShell {
             buildInputs =
